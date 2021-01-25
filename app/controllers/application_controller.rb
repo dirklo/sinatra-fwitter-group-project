26aefebea476504128c3510fec1d0f8be6af8c75
Rick <dirklo@gmail.com>
2021-01-25 19:41:47 +0100

    Worked on Application Controller
    Made some views and routes to pass rspec tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,55 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
   end
 
   get '/' do 
     erb :index
   end
-  
+
+  get '/login' do
+    if session[:user_id]
+      redirect '/tweets'
+    end
+
+    erb :'users/login'
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if user
+      session[:user_id] = user.id
+      redirect '/tweets'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/signup' do
+    if session[:user_id]
+      redirect '/tweets/index'
+    else
+      erb :'users/signup'
+    end
+  end
+
+  post '/signup' do
+    if params[:username].empty? || params[:email].empty? || params[:password].empty?
+      redirect '/signup'
+    else
+      user = User.create(username: params[:username], email: params[:email], password: params[:password]) 
+      session[:user_id] = user.id
+      redirect '/tweets' 
+    end
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/login'
+  end
+
+
 
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,8 +1,12 @@
 class TweetsController < ApplicationController
 
-    get '/tweets' do 
-        @tweets = Tweet.all
-        erb :'tweets/tweets'
+    get '/tweets' do
+        if !session[:user_id] 
+            redirect '/login' 
+        else
+            @tweets = Tweet.all
+            @user = User.find(session[:user_id])
+            erb :'tweets/index'
+        end
     end
-
 end
